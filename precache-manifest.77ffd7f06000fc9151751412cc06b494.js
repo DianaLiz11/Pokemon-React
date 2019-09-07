c4e496152e8302575164188a5c52569d80cfe718
DianaLiz11 <dianamunoz86@live.com.mx>
2019-09-06 19:38:03 -0500

    Updates

---
+++ precache-manifest.77ffd7f06000fc9151751412cc06b494.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7508cc509cee2bd8ec7ab5f7aeda277",
+    "url": "/Pokemon-React/index.html"
+  },
+  {
+    "revision": "112c8dcbacdf72dcc363",
+    "url": "/Pokemon-React/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "cef52859ade706504c1a",
+    "url": "/Pokemon-React/static/css/main.d872f1b3.chunk.css"
+  },
+  {
+    "revision": "112c8dcbacdf72dcc363",
+    "url": "/Pokemon-React/static/js/2.963fce50.chunk.js"
+  },
+  {
+    "revision": "cef52859ade706504c1a",
+    "url": "/Pokemon-React/static/js/main.58378d35.chunk.js"
+  },
+  {
+    "revision": "d1d4a0c6c8d27aca982b",
+    "url": "/Pokemon-React/static/js/runtime~main.e0105440.js"
+  },
+  {
+    "revision": "195f7da3c7c4056e3e3ca407ec0502a0",
+    "url": "/Pokemon-React/static/media/Type_Agua.195f7da3.png"
+  },
+  {
+    "revision": "354ce98fb659ecbcaa3b31531f6f2446",
+    "url": "/Pokemon-React/static/media/Type_Bicho.354ce98f.png"
+  },
+  {
+    "revision": "cf52290bc067f54e7ebd14853c3c9551",
+    "url": "/Pokemon-React/static/media/Type_Dragón.cf52290b.png"
+  },
+  {
+    "revision": "374b4c18424c0d38d8ac7afdd31eddc1",
+    "url": "/Pokemon-React/static/media/Type_Fantasma.374b4c18.png"
+  },
+  {
+    "revision": "bec0b844d8795048abb68ea35014a8b6",
+    "url": "/Pokemon-React/static/media/Type_Fuego.bec0b844.png"
+  },
+  {
+    "revision": "4a366c6986f4df4edb20ace6aa06aa08",
+    "url": "/Pokemon-React/static/media/Type_Hielo.4a366c69.png"
+  },
+  {
+    "revision": "cedd9513383912689cc41ff22fa059b6",
+    "url": "/Pokemon-React/static/media/Type_Lucha.cedd9513.png"
+  },
+  {
+    "revision": "ad854099be1eeed15a4d1e5368b09976",
+    "url": "/Pokemon-React/static/media/Type_Normal.ad854099.png"
+  },
+  {
+    "revision": "ac99ca63a3a9e98b08f8ef6edbf23135",
+    "url": "/Pokemon-React/static/media/Type_Planta.ac99ca63.png"
+  },
+  {
+    "revision": "35535d2cc6393905dc9315e8dfb6cdd4",
+    "url": "/Pokemon-React/static/media/Type_Psíquico.35535d2c.png"
+  },
+  {
+    "revision": "f837600c82044b17b7ec94ce990c13cc",
+    "url": "/Pokemon-React/static/media/Type_Roca.f837600c.png"
+  },
+  {
+    "revision": "f2c82b51fe79f6fd34b0cef50a32502e",
+    "url": "/Pokemon-React/static/media/Type_Tierra.f2c82b51.png"
+  },
+  {
+    "revision": "2f9959db566e7ccfc7a0d813e483bf6d",
+    "url": "/Pokemon-React/static/media/Type_Veneno.2f9959db.png"
+  },
+  {
+    "revision": "6dd88f48b7fb0527d38d3e493209a6cf",
+    "url": "/Pokemon-React/static/media/Type_Volador.6dd88f48.png"
+  },
+  {
+    "revision": "f00c9461a49f6cf3ffe9c192456db947",
+    "url": "/Pokemon-React/static/media/backInicio.f00c9461.jpg"
+  },
+  {
+    "revision": "166c3adb6c3150c596e8fd75aae6fb0a",
+    "url": "/Pokemon-React/static/media/pokemon-logo.166c3adb.png"
+  }
+]);
